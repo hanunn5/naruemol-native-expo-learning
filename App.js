7183c24999a18cb91aec442e6b0168129896cad5
naruemol <hanunn300@gmail.com>
2025-07-27 16:53:03 +0700

    feat: set up React Navigation with Home Details and Profile screens

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from "./components/Navigation";
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>นฤมล แน่นอุดร ทธด.102</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    textcolor: '#fff',
-    backgroundColor: '#b3e5fc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ pages/ProfileScreen.js
@@ -0,0 +1,16 @@
+import {View, Text, Button} from "react-native";
+export default function ProfileScreen ({navigation}){
+  return (
+    <View style={{flex: 1, alignItems: "center" , justifyContent: "center" }}>
+      <Text>Naruemol Naenaudorn DBT 102</Text>
+        <Button 
+      title="Go to Home" 
+      onPress={() => navigation.navigate("Home")} 
+      />
+        <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+     />
+    </View>
+  );
+}
