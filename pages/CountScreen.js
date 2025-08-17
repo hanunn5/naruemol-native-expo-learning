af2a22d443ab6381b0c91281c991f5e1153cd37d
naruemol <hanunn300@gmail.com>
2025-08-17 13:27:07 +0700

    feat: set up React Navigation with Home Details and Profile screens

--- pages/CountScreen.js
+++ pages/CountScreen.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button } from 'react-native';
 
 export default function CountScreen({ navigation }) {
   const [count, setCount] = useState(0);
 
+ 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>ค่าปัจจุบัน: {count}</Text>
 
       <View style={styles.buttonContainer}>
-        <Button
-          title="เพิ่มค่า"
-          onPress={() => setCount(count + 1)}
-        />
-        <View style={{ width: 20 }} />
-        <Button
-          title="ลบค่า"
-          onPress={() => setCount(count > 0 ? count - 1 : 0)}
-        />
+        {/* ปุ่มเพิ่มค่า */}
+        <TouchableOpacity onPress={() => setCount(count + 1)}>
+          <Image source={require('../assets/duck-button.png')} style={styles.duckButton} />
+        </TouchableOpacity>
+
+        <View style={{ width: 10 }} />
+
+        {/* ปุ่มลดค่า */}
+        <TouchableOpacity onPress={() => setCount(count > 0 ? count - 1 : 0)}>
+          <Image source={require('../assets/frog-button.png')} style={styles.frogButton} />
+        </TouchableOpacity>
       </View>
 
-      <View style={{ marginTop: 30 }}>
+      {/* ปุ่มกลับ Home (ยังคงใช้ Button ปกติ) */}
+      <View style={{ marginTop: 15 }}>
         <Button
           title="Home"
           onPress={() => navigation.navigate('Home')}
@@ -29,6 +33,7 @@ export default function CountScreen({ navigation }) {
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -44,5 +49,15 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+    duckButton: {
+    width: 150,   // ปรับขนาดตามที่ต้องการ เช่น 40, 50
+    height: 150,
+    resizeMode: 'contain',
+  },
+  frogButton: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
